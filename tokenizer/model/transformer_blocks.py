b9c8eb75b0fd50bbdf30027f5aacb0b1986ce2e7
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-11-03 03:02:52 +0900

    added temporal causal masking

--- tokenizer/model/transformer_blocks.py
+++ tokenizer/model/transformer_blocks.py
@@ -1,6 +1,7 @@
 # python tokenizer/model/transformer_blocks.py
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as data
 import numpy as np
@@ -35,3 +36,24 @@ class FeedForward(nn.Module):
         # Project back down
         x = self.dropout(self.down(x))
         return x
+
+def causal_masking_function(seq_len):
+    """
+    Small helper function used in temporal attention layers.
+    Creates mask that ensures the model only attends to past (and current) time step frames.
+    """
+    # Create a causal mask for attention
+    mask = torch.tril(torch.ones(seq_len, seq_len))
+    return mask  # Shape: (seq_len, seq_len)
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, input_size, num_heads, mask: bool, causal_masking_function):
+        super(MultiHeadAttention, self).__init__()
+        assert input_size % num_heads == 0 , "input_size must be divisible by num_heads"
+        self.head_dim = input_size // num_heads
+        self.w_q = nn.Linear(input_size, input_size) # Query projection
+        self.w_k = nn.Linear(input_size, input_size) # Key projection
+        self.w_v = nn.Linear(input_size, input_size) # Value projection
+        self.w_out = nn.Linear(input_size, input_size) # Output projection
+        self.dropout = nn.Dropout(0.1)
+        self.causal_masking = causal_masking_function
