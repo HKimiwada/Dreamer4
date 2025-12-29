61c5d24d39256e7d38c93bb4a7b5e9f67acaa097
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-12-29 10:05:45 +0900

    deleted code

--- imagination_training/wm_heads.py
+++
@@ -1,4 +0,0 @@
-# Code to store policy head, value head, reward head for imagination-augmented agents.
-# Policy head: outputs action distribution from latent z_t
-# Value head: estimates value of z_t (TD target inside imagination)
-# Reward head: if the pretrained MineWorld doesn’t predict rewards, train a small reward net
